eb8e30dd168d5fe65dbd0bb0f4168bde5f6ee7f8
imdutch21 <bartpklomp@gmail.com>
2014-08-22 12:03:40 +0200

    made stuff work
    the oilcompressor should work now

--- src/main/java/bart/oilcraft/tileentities/OilCompressorEntity.java
+++ src/main/java/bart/oilcraft/tileentities/OilCompressorEntity.java
@@ -2,6 +2,8 @@ package bart.oilcraft.tileentities;
 
 import bart.oilcraft.blocks.ModBlocks;
 import bart.oilcraft.containers.ContainerOilCompressor;
+import bart.oilcraft.items.ModItems;
+import bart.oilcraft.util.Util;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -10,6 +12,7 @@ import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -24,6 +27,8 @@ public class OilCompressorEntity extends TileEntity implements ISidedInventory{
 
     public ItemStack[] items = new ItemStack[3];
 
+    public int oilLevel, progress;
+
     @Override
     public int[] getAccessibleSlotsFromSide(int side) {
         return side == 0 ? slotsExtract : slotsInsert;
@@ -32,7 +37,7 @@ public class OilCompressorEntity extends TileEntity implements ISidedInventory{
     @Override
     public boolean canInsertItem(int slot, ItemStack stack, int side) {
 
-        System.out.println("Item 1: " + stack.getItem() + "  Item 2: " + Item.getItemFromBlock(Blocks.cobblestone));
+        //System.out.println("Item 1: " + stack.getItem() + "  Item 2: " + Item.getItemFromBlock(Blocks.cobblestone));
 
         return (slot == 0 && (stack.getItem() == Item.getItemFromBlock(Blocks.cobblestone) || Item.getIdFromItem(stack.getItem()) == Block.getIdFromBlock(ModBlocks.CrudeOilOre))) ||
                (slot == 1 && stack.getItem() == Items.bucket);
@@ -134,6 +139,70 @@ public class OilCompressorEntity extends TileEntity implements ISidedInventory{
                (slot == 1 && stack.getItem() == Items.bucket);
     }
 
+    @Override
+    public void updateEntity() {
+        if (worldObj.isRemote) return;
+        if (getOilLevel(items[0]) > 0) {
+            if (progress >= 100) {
+                if (oilLevel + getOilLevel(items[0]) <= 10000) {
+                    progress = 0;
+                    oilLevel += getOilLevel(items[0]);
+                    setInventorySlotContents(0, items[0].stackSize == 1 ? null : new ItemStack(items[0].getItem(), items[0].stackSize - 1));
+                }
+            }
+            else {
+                progress++;
+            }
 
+        }
+        else
+        {
+            progress = 0;
+        }
 
+        if (oilLevel >= 1000 && items[1] != null && items[1].getItem() == Items.bucket && items[2] == null) {
+            oilLevel -= 1000;
+            setInventorySlotContents(1, items[1].stackSize == 1 ? null : new ItemStack(items[1].getItem(), items[1].stackSize - 1));
+            setInventorySlotContents(2, new ItemStack(ModItems.OilBucket));
+        }
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt)
+    {
+        super.readFromNBT(nbt);
+
+        oilLevel = nbt.getInteger("oilLevel");
+
+        Util.loadInventory(nbt, this);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt)
+    {
+        super.writeToNBT(nbt);
+
+        nbt.setInteger("oilLevel", oilLevel);
+
+        Util.saveInventory(nbt, this);
+    }
+
+    public static int getOilLevel(ItemStack stack)
+    {
+        if (stack == null)
+        {
+            return 0;
+        }
+        else {
+
+            if (stack.getItem() == Item.getItemFromBlock(Blocks.cobblestone)) {
+                return 1;
+            }
+            if (stack.getItem() == Item.getItemFromBlock(ModBlocks.CrudeOilOre)) {
+                return 1000;
+            }
+
+        }
+        return 0;
+    }
 }
