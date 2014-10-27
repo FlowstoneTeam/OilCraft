f8a9e5da15ddcfe850694427ab4480678c81ea24
imdutch21 <bartpklomp@gmail.com>
2014-10-27 14:52:43 +0100

    crafting

--- src/main/java/bart/oilcraft/lib/handler/CraftingHandler.java
+++ src/main/java/bart/oilcraft/lib/handler/CraftingHandler.java
@@ -2,6 +2,7 @@ package bart.oilcraft.lib.handler;
 
 import bart.oilcraft.blocks.ModBlocks;
 import bart.oilcraft.items.ModItems;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -17,6 +18,11 @@ public class CraftingHandler {
         CraftingManager.getInstance().addRecipe(new ItemStack(ModBlocks.OilCompressor), "IPI", "I I", "IAI", 'P', Blocks.piston, 'I', Items.iron_ingot, 'A', ModItems.EnergyAcceptor);
         CraftingManager.getInstance().addRecipe(new ItemStack(ModBlocks.OilGenerator), "IGI", "I I", "IAI", 'I', Items.iron_ingot, 'G', Items.gold_ingot, 'A', ModItems.EnergyAcceptor);
         CraftingManager.getInstance().addRecipe(new ItemStack(ModItems.EnergyAcceptor), "RRR", "RGR", "RRR", 'R', Items.redstone, 'G', Items.gold_nugget);
+
+        if(GameRegistry.findItem("thermalexpansion", "powerCoilGold") != null){
+            CraftingManager.getInstance().addRecipe(new ItemStack(ModBlocks.OilCompressor), "IPI", "I I", "IAI", 'P', Blocks.piston, 'I', Items.iron_ingot, 'A', GameRegistry.findItem("thermalexpansion", "powerCoilGold"));
+            CraftingManager.getInstance().addRecipe(new ItemStack(ModBlocks.OilGenerator), "IGI", "I I", "IAI", 'I', Items.iron_ingot, 'G', Items.gold_ingot, 'A', GameRegistry.findItem("thermalexpansion", "powerCoilGold"));
+        }
     }
 
 }
